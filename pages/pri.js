8e6bb8ca964dcb52cdff8169d4ce14a76fd4d4ac
holguin4857 <omar@trexco.net>
2023-03-02 14:14:55 +0100

    pri4

--- pages/pri.js
+++ pages/pri.js
@@ -11,19 +11,29 @@ export default function Pri() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div>
-      <h1>Privacy policy</h1>
+      <section className="container">
+        <div className="content">
+          <h1>Privacy policy</h1>
           <p></p>
 
           <p>
             This privacy policy sets out how Trexco uses and protects any
             information that you give when you use this website.
           </p>
-
-      </div>
-
-      
+          <p>
+            Trexco is committed to ensuring that your privacy is protected.
+            Should we ask you to provide certain information by which you can be
+            identified when using this website, then you can be assured that it
+            will only be used in accordance with this privacy statement.
+          </p>
+          <p>
+            Trexco may change this policy from time to time by updating this
+            page. You should check this page from time to time to ensure that
+            you are happy with any changes. This policy is effective from
+            24/11/2017
+          </p>
+        </div>
+      </section>
     </>
   );
 }

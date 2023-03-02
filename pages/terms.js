9108925b590c979cc6ee09ebeac4bd4e321acd85
holguin4857 <omar@trexco.net>
2023-03-02 16:17:29 +0100

    terms5

--- pages/terms.js
+++ pages/terms.js
@@ -99,13 +99,348 @@ export default function Terms() {
           <p>
             2.2 - These terms do not affect your statutory rights as a consumer.
           </p>
-          <ol></ol>
-
+          <ol>
+            <h2> Prices</h2>
+          </ol>
           <p>
-            You can choose to accept or decline cookies. Most web browsers
-            automatically accept cookies, but you can usually modify your
-            browser setting to decline cookies if you prefer. This may prevent
-            you from taking full advantage of the website.
+            3.1 - The price of the Services shall subject to clause 4.5 be
+            Trexco stated price, or where no price has been stated or where a
+            stated price is no longer valid, such price as Trexco shall notify
+            the Customer. Where the Customer provides to Trexco a collection
+            date more than 15 days from placing the order for shipping, Trexco
+            shall be entitled to increase any price stated.
+          </p>
+          <p>
+            3.2 - Trexco reserves the right, by giving written notice to the
+            Customer to increase the price of the Services to reflect any
+            increase which is due to any factor beyond the control of Trexco
+            such as without limitation, changes in the Delivery Address or any
+            changes instigated by the Customer any foreign exchange rate
+            fluctuation, currency regulation, alteration of duties, significant
+            increase in the cost of labour, duties or taxes levied by any
+            governmental authority or statutory body and any change in delivery
+            date or Delivery Address.
+          </p>
+          <p>
+            3.3 - Trexco offers no guarantee to the charges applicable at the
+            destination. Destination charges are outside of the control of
+            Trexco and any figures quoted by Trexco are an indication only.
+          </p>
+          <p>
+            3.4 - Unless otherwise specified insurance where opted for by the
+            Customer, VAT and any other tax, duty or charge payable by the
+            Customer shall be added to the price stated.
+          </p>
+          <ol>
+            <h2> Terms of Payment</h2>
+          </ol>
+          <p>4.1 - All prices are in UK GBP</p>
+          <p>
+            4.2 - Trexco may provide the Customer with an invoice for the price
+            of the Services on or at any time after delivery of the Boxes.
+          </p>
+          <p>
+            4.3 - No payment shall be deemed to have been received until Trexco
+            has received in full cleared funds in UK GBP.
+          </p>
+          <p>
+            4.4 - Trexco is entitled to offset any amount owing to it from the
+            Customer against any amount owed to the Customer by Trexco.
+          </p>
+          <p>
+            4.5 - If the Customer fails to make any payment in whole or in part
+            on the due date or is otherwise in breach of these Terms, then
+            without limiting any other right or remedy available to Trexco,
+            Trexco may:
+          </p>
+          <p>
+            4.5.1 cancel the Contract or suspend or withhold delivery of the
+            Boxes.
+          </p>
+          <p>
+            4.5.2 charge the Customer interest both before and after any
+            judgement on the amount unpaid, at the rate of 4 percent per annum
+            above HSBC plc base rate from time to time, until payment in full is
+            made a part month being treated as a full month for the purpose of
+            calculating interest.
+          </p>
+          <ol></ol>
+          <ol>
+            <h2> Additional Charges</h2>
+          </ol>
+          <p>
+            5.1 - Trexco reserves the right to make and the Customer hereby
+            authorises Trexco to make a charge or charges to the Customer credit
+            card or debit card where payment is required in the following
+            circumstances 24 hours notice of such a charge will be given:
+          </p>
+          <p>
+            5.1.1 a Box exceeds the weight set out in these terms a charge of 30
+            percent above the rate for the whole consignment of Boxes shall
+            apply. Minimum charge 30.00 GBP.
+          </p>
+          <p>
+            5.1.2 a Box requires re-packing by Trexco because the Customer has
+            packed a Prohibited Item or has failed to pack the Box in a manner
+            prescribed by Trexco, a charge of 20 percent above the premium rate
+            charged in respect of the first Box shall apply, as well as
+            additional labour charged at 45.00 GBP per hour.
+          </p>
+          <p>5.1.3 an administration fee of 35.00 GBP for refunds.</p>
+          <p>
+            5.1.4 storage or disposal costs where delivery has been detained by
+            a government or regulatory authority or where the Customer was not
+            at the Delivery Address at the time of delivery or delivery could
+            not be made due to reasons beyond the control of Trexco.
+          </p>
+          <p>
+            5.1.5 the Customer cargo is incurring charges for storage prior to
+            shipment being made.
+          </p>
+          <ol>
+            <h2> Box specifications</h2>
+          </ol>
+          <p>6.1 - Boxes must not unless agreed in writing by Trexco:</p>
+          <p>
+            6.1.1 Exceed the agreed weight: Trexx Box = 600kg, FCL shipping
+            container = 27000 kg.
+          </p>
+          <p>6.1.2 contains a Prohibited Item.</p>
+          <p>
+            6.1.3 be packed in such a way that compromises the structure of the
+            box.
+          </p>
+          <p>
+            6.1.4 be assembled without having followed the instructions given by
+            Trexco.
+          </p>
+          <p>
+            6.1.4 in the case of a shipping container, be loaded in accordance
+            with regulations at both origin and destination, this is inclusive
+            of but not limited to the weight and cargo distribution. Any charges
+            consequential to overweight or incorrectly stowed containers will
+            remain for the account of the customer.
+          </p>
+          <ol>
+            <h2> Collection</h2>
+          </ol>
+          <p>
+            7.1 - The Customer shall accurately complete Trexco online
+            collection and destination details form.
+          </p>
+          <p>
+            7.2 - The Customer must ensure that all Boxes are placed on the
+            ground at the front of the Collection Address, as near to the road
+            as possible ready for collection. Collection may be refused or
+            additional charges may be charged to the Customer if this condition
+            is not met.
+          </p>
+          <p>
+            7.3 - The collection will be at kerbside and the boxes must have
+            free access to a pallet truck.
+          </p>
+          <p>
+            7.4 - In case of a shipping container, the customer shall notify the
+            relevant authorities to provide parking space for loading or
+            unloading.
+          </p>
+          <p>
+            7.5 - Trexco must receive notification from the Customer of any
+            change to the Delivery Address before Trexco collects the Boxes.
+          </p>
+          <h2>7.6 - The Customer shall ensure that:</h2>
+          <p>
+            7.6.1 The Customer shall provide the collection receipt to the
+            carrier for signing when the Boxes are collected. The carrier shall
+            return the collection receipt to the Customer confirming the number
+            of Boxes collected. Should any collection receipt not be presented
+            to the Carrier, Trexco shall be entitled to rely on the evidence of
+            the carrier as to the number of Boxes collected.
+          </p>
+          <p>
+            7.6.2 In case of a shipping container, a minimum of 15 mts of
+            parking and loading space for the lorry is available.
+          </p>
+          <h2>
+            7.7 - Trexco retains the right to refuse collection or positioning
+            of any Box which:
+          </h2>
+          <p>
+            7.7.1 Does not meet the box specifications set out in clause 6.1 or
+            Trexco in its entire discretion deems unsuitable for transportation.
+          </p>
+          <p>7.7.3 Loading area is not accessible or suitable for loading.</p>
+          <ol>
+            <h2> Delivery</h2>
+          </ol>
+          <p>
+            8.1 - Trexco shall deliver the Boxes to the Delivery Address or port
+            or airport arranged in accordance with the contract.
+          </p>
+          <p>
+            8.2 - All destination charges are for the account of the customer,
+            the customer shall ensure at the country of destination that customs
+            formalities, fees, penalties and any charge related to the delivery
+            of the cargo is promptly paid and dealt with.
+          </p>
+          <p>
+            8.3 - The Customer shall inspect the Boxes at the Delivery Address.
+            The Customer must record any damage to the Boxes or missing Boxes on
+            the delivery note. If the Customer fails to sign and annotate the
+            delivery note confirming all Boxes are present and undamaged, Trexco
+            are unable to claim against the carrier and the Customer will be
+            responsible for any replacement costs where damage has been caused
+            to the Boxes.
+          </p>
+          <p>
+            8.4 - The customer will not in all circumstances be entitled to
+            compensation, or to full compensation. For any loss and is therefore
+            recommended to seek professional advice as to appropriate insurance
+            cover to be maintained while consignments are in transit.
+          </p>
+          <p>
+            8.5 - Where Trexco is unable to deliver the Boxes to the Delivery
+            Address, Trexco shall inform the Customer in writing and shall have
+            the right to:
+          </p>
+          <p>8.5.1 store the Boxes at the Customers expense or:</p>
+          <p>
+            8.5.2 destroy or dispose of the Boxes and their contents at the
+            Customer expense within 30 days of notifying the Customer of the
+            failure to deliver.
+          </p>
+          <p>
+            8.6 - The Customer shall pay Trexco on demand any storage fees,
+            demurrage fees, port charges and associated costs that arise under
+            clauses 6 and 7.
+          </p>
+          <p>
+            8.7 - The Customer shall at all times act in good faith in its
+            dealings with Trexco.
+          </p>
+
+          <ol>
+            <h2> Liability</h2>
+          </ol>
+          <p>
+            9.1 - The following provisions set out the entire financial
+            liability of Trexco including any liability for the act or omissions
+            of its employees, agents and subcontractors to the Customer in
+            respect of any breach of these Terms and any representation,
+            statement or tortious act or omission including negligence arising
+            under or in connection with the Contract:
+          </p>
+          <p>
+            9.1.1 Trexco total liability in contract, including negligence or
+            breach of statutory duty, misrepresentation, restitution or
+            otherwise arising in connection with the performance or contemplated
+            performance of this Contract shall be limited to 100 GBP per Box
+            subject to a maximum of the Contract price;
+          </p>
+          <p>
+            9.1.2 Trexco shall not be liable to the Customer for any indirect or
+            consequential loss or damage whether for loss of profit, loss of
+            business, depletion of goodwill or otherwise, costs, expenses or
+            other claims for consequential compensation whatsoever howsoever
+            caused which arise out of or in connection with the Contract.
+          </p>
+          <ol>
+            <h2> Force Majeure</h2>
+          </ol>
+          <p>
+            10.1 - If Trexco is prevented or hindered or delayed from providing
+            the Services in accordance with these Terms by a Force Majeure
+            Event, Trexco may:
+          </p>
+          <p>
+            10.1.1 suspend delivery of the Boxes to the Delivery Address while
+            the Force Majeure Event continues or -
+          </p>
+          <p>
+            10.1.2 terminate the contract with immediate effect by written
+            notice to the Customer and Trexco shall not be liable to the
+            Customer for any indirect or consequential loss or damage whether
+            for loss of profit, loss of business, depletion of goodwill or
+            otherwise, costs, expenses or other claims for consequential
+            compensation whatsoever howsoever caused as a result.
+          </p>
+          <ol>
+            <h2> Parties and Subcontractors</h2>
+          </ol>
+          <p>
+            11.1 - The Customer warrants that he is either the owner of the
+            content of the boxes or is authorised by such owner to accept these
+            Conditions on such owner behalf.
+          </p>
+          <p>
+            11.2 - The carriage of any Boxes by rail, sea, inland waterway or
+            air is arranged by Trexco as agent of the Customer and shall be
+            subject to the Conditions of the rail, sea, road or air carrier
+            contracted to carry the Boxes. Trexco shall be under no liability
+            whatever to whomsoever and howsoever arising in respect of such
+            carriage, nor shall be liable for any loss, damage or delay shall be
+            deemed to have occurred while the Box was being carried.
+          </p>
+          <p>
+            11.3 -Trexco may in its absolute discretion subcontract all or any
+            part of the Services to a third party.
+          </p>
+
+          <ol>
+            <h2> Severability</h2>
+          </ol>
+          <p>
+            12.1 - If any Term of a Contract or part of a Term, is found by any
+            court or administrative body of competent jurisdiction to be
+            invalid, unenforceable or illegal, the other Terms shall remain in
+            force.
+          </p>
+          <p>
+            12.2 - If any invalid, unenforceable or illegal Term of a Contract
+            would be valid, enforceable or legal if some part of it were
+            deleted, the Term shall apply with whatever modification is
+            necessary to give effect to the commercial intentions of Trexco.
+          </p>
+          <ol>
+            <h2> Waiver</h2>
+          </ol>
+          <p>
+            13.1 -The failure by Trexco to exercise or delay by Trexco in
+            exercising any right, power or remedy provided by a Contract or by
+            law does not constitute a waiver of such right, power or remedy or a
+            waiver of any other rights, powers, or remedies. No single or
+            partial exercise of a right, power or remedy provided by a Contract
+            shall prevent any further exercise of the right, power or remedy or
+            the exercise of another right, power or remedy,
+          </p>
+          <ol>
+            <h2> Third Party Rights</h2>
+          </ol>
+          <p>
+            14.1 - Nothing in a Contract is intended to confer on any person any
+            right to enforce any Term of Contract which that person would not
+            have had but for the Contracts Rights of Third Parties Act 1999.
+          </p>
+          <ol>
+            <h2> Governing Law and Jurisdiction</h2>
+          </ol>
+          <p>
+            15.1 - A Contract is governed by, and shall be construed in
+            accordance with, English law.
+          </p>
+          <p>
+            15.2 - The courts of England have exclusive jurisdiction to hear and
+            decide any suit, action or proceedings and to settle any disputes,
+            which may arise out of or in connection with a Contract and, for
+            these purposes, each party irrevocably submits to the jurisdiction
+            of the courts of England.
+          </p>
+          <ol>
+            <h2> Notice</h2>
+          </ol>
+          <p>
+            16.1 - A Notice required or permitted to be given by the Customer to
+            Trexco under these terms shall be in writing.
           </p>
         </div>
       </section>
